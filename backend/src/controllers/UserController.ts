59ace826c1afa2d24ee6011e8fea4b4eb88ed81c
Jayden Melendez <jayden.melendez1@marist.edu>
2025-09-30 09:11:48 -0400

    Created a create user controller, created controller folder, created user controller file

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,15 @@
+import type { Request, Response } from 'express';
+import { default as User } from '../models/user';
+
+// Create a new user
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const userData = req.body;
+        const newUser = new User(userData);
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
