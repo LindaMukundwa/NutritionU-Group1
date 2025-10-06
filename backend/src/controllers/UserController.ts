bce0a7dbfdae4248324544b29f033a3dbe0f7025
Jayden Melendez <jayden.melendez1@marist.edu>
2025-10-06 13:11:42 -0400

    Get user route added

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
-import { default as User } from '../models/user';
+import { default as User } from '../models/user.ts';
+import { error } from 'console';
 
 // Create a new user
 export const createUser = async (req: Request, res: Response) => {
@@ -13,3 +14,18 @@ export const createUser = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 };
+
+// Get an existing user
+export const getUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const userData = await User.findById(id);
+        if (!userData) {
+            throw error("User with this id is not found");
+        } 
+        res.status(201).json(userData);
+    } catch(error) {
+        console.log('Error getting user:', error);
+        res.status(500).json({ message: 'Internal server error'});
+    }
+}
--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
-import { createUser } from '../controllers/UserController.ts';
+import { createUser, getUser } from '../controllers/UserController.ts';
 
 const router = Router();
 
 // Route to create a new user
 router.post('/create', createUser);
+
+router.get('/:id', getUser);
 
 export default router;
